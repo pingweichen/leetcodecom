6271b8102e092803b5e9075600274a34c55e5ebc
pingweichen <cpwei120@sina.com>
2022-05-19 16:44:46 +0800

    first easy

---
+++ 136.single-number.c
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode id=136 lang=c
+ *
+ * [136] Single Number
+ */
+
+// @lc code=start
+
+
+
+int singleNumber(int* nums, int numsSize)
+{
+    for (int i = 0; i < numsSize; i++)
+    {
+        
+    }
+
+
+    return -1;
+}
+
+// @lc code=end
---
+++ 53.maximum-subarray.c
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=53 lang=c
+ *
+ * [53] Maximum Subarray
+ */
+
+// @lc code=start
+
+
+int maxSubArray(int* nums, int numsSize){
+    
+}
+// @lc code=end
+
