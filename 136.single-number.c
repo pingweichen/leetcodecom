6dd82a1a8781983cd30d06b679375f20824e9486
pingweichen <cpwei120@sina.com>
2022-05-27 11:46:37 +0800

    yes

--- 136.single-number.c
+++ 136.single-number.c
@@ -8,15 +8,39 @@
 
 
 
+// int singleNumber(int* nums, int numsSize)
+// {
+//     int n = nums[0];
+//     for (int i = 1; i < numsSize; i++)
+//     {
+//         n = n ^ nums[i];
+//     }
+    
+//     return n;
+// }
+
+// @lc code=end
+
+
+
 int singleNumber(int* nums, int numsSize)
 {
     for (int i = 0; i < numsSize; i++)
-    {
+    {   int count = 0;
+        for (int j = 0; j < numsSize; j++)
+        {
+            if (nums[i] == nums[j])
+            {
+                count++;
+            }
+            
+        }
+        if (count == 1)
+        {
+            return nums[i];
+        }
         
     }
 
-
-    return -1;
+    return -1;   
 }
-
-// @lc code=end
